c10bc7eb70f22ec31cb4344d36abdf9949388ccb
Bruno Rosendo <brunomrosendo@hotmail.com>
2022-04-11 15:02:56 +0100

    Refactored library search header

--- src/app_feup/lib/view/Pages/library_page_view.dart
+++ src/app_feup/lib/view/Pages/library_page_view.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:uni/view/Pages/secondary_page_view.dart';
-import 'package:uni/view/Widgets/form_text_field.dart';
+import 'package:uni/view/Widgets/library_search_header.dart';
 
 class LibraryPage extends StatefulWidget {
   @override
@@ -11,74 +11,6 @@ class LibraryPage extends StatefulWidget {
 class LibraryPageState extends SecondaryPageViewState {
   @override
   Widget getBody(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.fromLTRB(20, 20, 20, 400),
-      alignment: Alignment.center,
-      child: Column(children: <Widget>[
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text(
-              'Biblioteca',
-              style:
-                  Theme.of(context).textTheme.headline6.apply(fontSizeDelta: 7),
-            ),
-            Material(
-                child: ElevatedButton(
-                    child: Text('Reservas'),
-                    onPressed: () {
-                      debugPrint('clicked reservations');
-                    })),
-          ],
-        ),
-        SizedBox(height: 15),
-        Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Flexible(
-                child: TextFormField(
-                  style: TextStyle(color: Colors.white, fontSize: 20),
-                  enableSuggestions: false,
-                  autocorrect: false,
-                  autofocus: false,
-                  controller: TextEditingController(),
-                  focusNode: null, // TODO
-                  onFieldSubmitted: (term) {
-                    // TODO
-                  },
-                  textInputAction: null, // TODO
-                  textAlign: TextAlign.left,
-                  decoration: InputDecoration(
-                      icon: Icon(Icons.search),
-                      hintText: 'Penis',
-                      labelText: 'Procure',
-                      contentPadding: EdgeInsets.fromLTRB(10, 0, 10, 0),
-                      enabledBorder: const UnderlineInputBorder(
-                          borderSide:
-                              BorderSide(color: Colors.black, width: 1)),
-                      focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(
-                              color: Theme.of(context).accentColor, width: 2))),
-                  validator: (String value) =>
-                      value.isEmpty ? 'Preenche este campo' : null,
-                ),
-              ),
-              Container(
-                padding: EdgeInsets.only(left: 10),
-                child: IconButton(
-                  icon: Icon(Icons.tune, color: Theme.of(context).accentColor),
-                  onPressed: () {
-                    debugPrint('clicked icon');
-                  },
-                  tooltip: 'Advanced Search',
-                  iconSize: 28,
-                  alignment: Alignment.center,
-                  padding: EdgeInsets.fromLTRB(15, 0, 15, 0),
-                ),
-              )
-            ])
-      ]),
-    );
+    return LibrarySearchHeader();
   }
 }
---
+++ src/app_feup/lib/view/Widgets/library_search_header.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class LibrarySearchHeader extends StatelessWidget {
+  static final FocusNode searchNode = FocusNode();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.fromLTRB(20, 20, 20, 400), // TODO Change padding
+      alignment: Alignment.center,
+      child: Column(children: <Widget>[
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              'Biblioteca',
+              style:
+                  Theme.of(context).textTheme.headline6.apply(fontSizeDelta: 7),
+            ),
+            Material(
+                child: ElevatedButton(
+                    child: Text('Reservas'),
+                    onPressed: () {
+                      debugPrint('clicked reservations');
+                    })),
+          ],
+        ),
+        SizedBox(height: 15),
+        createSearchOptions(context),
+      ]),
+    );
+  }
+
+  Widget createSearchOptions(BuildContext context) {
+    return Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [createSearchBar(context), createSearchFilters(context)]);
+  }
+
+  Widget createSearchBar(BuildContext context) {
+    return Flexible(
+      child: TextFormField(
+        style: TextStyle(color: Colors.white, fontSize: 20),
+        controller: TextEditingController(),
+        autofocus: false,
+        focusNode: searchNode,
+        onFieldSubmitted: (term) {
+          searchNode.unfocus();
+          // TODO Search action
+        },
+        textInputAction: TextInputAction.done,
+        textAlign: TextAlign.left,
+        decoration: searchBarInputDecoration(context, 'Procure'),
+        validator: (String value) =>
+            value.isEmpty ? 'Preencha este campo' : null,
+      ),
+    );
+  }
+
+  Widget createSearchFilters(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(left: 10),
+      child: IconButton(
+        icon: Icon(Icons.tune, color: Theme.of(context).accentColor),
+        onPressed: () {
+          // TODO Filter menu
+          debugPrint('clicked filter icon');
+        },
+        tooltip: 'Filtros de Pesquisa',
+        iconSize: 28,
+        alignment: Alignment.center,
+        padding: EdgeInsets.fromLTRB(15, 0, 15, 0),
+      ),
+    );
+  }
+
+  InputDecoration searchBarInputDecoration(
+      BuildContext context, String placeholder) {
+    return InputDecoration(
+        icon: Icon(Icons.search),
+        hintText: placeholder,
+        contentPadding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+        border: const UnderlineInputBorder(
+            borderSide: BorderSide(color: Colors.black, width: 1)),
+        focusedBorder: OutlineInputBorder(
+            borderSide:
+                BorderSide(color: Theme.of(context).accentColor, width: 2)));
+  }
+}
