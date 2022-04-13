244393d2dca5794c8f26eee3989890bd62f8d67a
laureansvictor <laureans.victor@gmail.com>
2022-04-13 20:07:55 +0100

    just ideas to do the scrapping

---
+++ src/app_feup/lib/controller/parsers/parser_library.dart
@@ -0,0 +1,8 @@
+// import 'dart:async';
+
+// import 'package:http/http.dart' as http;
+// import 'package:html/parser.dart' show parse;
+// import 'package:html/dom.dart';
+
+import 'package:web_scraper/web_scraper.dart';
+
