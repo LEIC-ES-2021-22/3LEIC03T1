04607fd0cbda0780da680892abc892563355c7c7
Bruno Rosendo <brunomrosendo@hotmail.com>
2022-04-25 22:01:25 +0100

    Overrided toString, equals and hashcode for book model

--- src/app_feup/lib/model/entities/book.dart
+++ src/app_feup/lib/model/entities/book.dart
@@ -1,3 +1,5 @@
+import 'package:collection/collection.dart';
+
 class Book {
   String title;
   String author;
@@ -34,4 +36,50 @@ class Book {
       this.documentType,
       this.isbnCode,
       this.themes});
+
+  @override
+  String toString() {
+    return '''$title - $author - $editor - $releaseYear - $language - $country - \
+      $unitsAvailable - $totalUnits - $hasPhysicalVersion - $hasDigitalVersion - \
+      $digitalURL - $imageURL - $documentType - $isbnCode - $themes''';
+  }
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is Book &&
+          runtimeType == other.runtimeType &&
+          title == other.title &&
+          author == other.author &&
+          editor == other.editor &&
+          ListEquality().equals(themes, other.themes) &&
+          releaseYear == other.releaseYear &&
+          language == other.language &&
+          country == other.country &&
+          unitsAvailable == other.unitsAvailable &&
+          totalUnits == other.totalUnits &&
+          hasPhysicalVersion == other.hasPhysicalVersion &&
+          hasDigitalVersion == other.hasDigitalVersion &&
+          digitalURL == other.digitalURL &&
+          imageURL == other.imageURL &&
+          documentType == other.documentType &&
+          isbnCode == other.isbnCode;
+
+  @override
+  int get hashCode =>
+      title.hashCode ^
+      author.hashCode ^
+      editor.hashCode ^
+      ListEquality().hash(themes) ^
+      releaseYear.hashCode ^
+      language.hashCode ^
+      country.hashCode ^
+      unitsAvailable.hashCode ^
+      totalUnits.hashCode ^
+      hasPhysicalVersion.hashCode ^
+      hasDigitalVersion.hashCode ^
+      digitalURL.hashCode ^
+      imageURL.hashCode ^
+      documentType.hashCode ^
+      isbnCode.hashCode;
 }
