44431f90807e367dd3f81e743c4b9445765ec004
monkin77 <monkinsmurf@gmail.com>
2022-04-30 15:42:52 +0100

    Created BookReservation entity

---
+++ src/app_feup/lib/model/entities/book_reservation.dart
@@ -0,0 +1,90 @@
+import 'package:collection/collection.dart';
+
+class BookReservation {
+  String title;
+  String author;
+  String editor;
+  String releaseYear;
+  String language;
+  String country;
+
+  int unitsAvailable;
+  int totalUnits;
+
+  bool hasPhysicalVersion;
+  bool hasDigitalVersion;
+
+  String digitalURL;
+  String imageURL;
+  String documentType;
+  String isbnCode;
+  List<String> themes;
+
+  BookReservation(
+      {this.title,
+      this.author,
+      this.editor,
+      this.releaseYear,
+      this.language,
+      this.country,
+      this.unitsAvailable,
+      this.totalUnits,
+      this.hasPhysicalVersion,
+      this.hasDigitalVersion,
+      this.digitalURL,
+      this.imageURL,
+      this.documentType,
+      this.isbnCode,
+      this.themes});
+
+  String getUnitsText() {
+    if (unitsAvailable == 1) return '1 unidade';
+    return '$unitsAvailable unidades';
+  }
+
+  @override
+  String toString() {
+    return '''$title - $author - $editor - $releaseYear - $language - $country - \
+      $unitsAvailable - $totalUnits - $hasPhysicalVersion - $hasDigitalVersion - \
+      $digitalURL - $imageURL - $documentType - $isbnCode - $themes''';
+  }
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is BookReservation &&
+          runtimeType == other.runtimeType &&
+          title == other.title &&
+          author == other.author &&
+          editor == other.editor &&
+          ListEquality().equals(themes, other.themes) &&
+          releaseYear == other.releaseYear &&
+          language == other.language &&
+          country == other.country &&
+          unitsAvailable == other.unitsAvailable &&
+          totalUnits == other.totalUnits &&
+          hasPhysicalVersion == other.hasPhysicalVersion &&
+          hasDigitalVersion == other.hasDigitalVersion &&
+          digitalURL == other.digitalURL &&
+          imageURL == other.imageURL &&
+          documentType == other.documentType &&
+          isbnCode == other.isbnCode;
+
+  @override
+  int get hashCode =>
+      title.hashCode ^
+      author.hashCode ^
+      editor.hashCode ^
+      ListEquality().hash(themes) ^
+      releaseYear.hashCode ^
+      language.hashCode ^
+      country.hashCode ^
+      unitsAvailable.hashCode ^
+      totalUnits.hashCode ^
+      hasPhysicalVersion.hashCode ^
+      hasDigitalVersion.hashCode ^
+      digitalURL.hashCode ^
+      imageURL.hashCode ^
+      documentType.hashCode ^
+      isbnCode.hashCode;
+}
