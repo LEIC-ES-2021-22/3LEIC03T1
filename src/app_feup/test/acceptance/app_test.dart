fe4d7769c0d463d621729f949f95007450a6cbda
Bruno Rosendo <brunomrosendo@hotmail.com>
2022-05-03 02:36:29 +0100

    Added library search feature example and navigate step

--- src/app_feup/test/acceptance/app_test.dart
+++ src/app_feup/test/acceptance/app_test.dart
@@ -3,15 +3,17 @@ import 'package:flutter_gherkin/flutter_gherkin.dart';
 import 'package:gherkin/gherkin.dart';
 import 'package:glob/glob.dart';
 
+import 'steps/navigate.dart';
+
 Future<void> main() {
   final config = FlutterTestConfiguration()
-    ..features = [Glob(r'test/acceptance/features/**.feature')]
+    ..features = [Glob('test/acceptance/features/**.feature')]
     ..reporters = [
       ProgressReporter(),
       TestRunSummaryReporter(),
-      JsonReporter(path: './test/acceptance/report.json')
+      JsonReporter(path: 'test/acceptance/report.json')
     ]
-    ..stepDefinitions = []
+    ..stepDefinitions = [navigate()]
     ..customStepParameterDefinitions = []
     ..restartAppBetweenScenarios = true
     ..targetAppPath = 'test/acceptance/app.dart';
