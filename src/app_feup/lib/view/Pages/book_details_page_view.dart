fa9bef8ec54abbb427e38ebda03ffb4e71672e51
Victor Nunes <victorsnunes99@gmail.com>
2022-05-11 14:44:19 +0100

    date input basic

--- src/app_feup/lib/view/Pages/book_details_page_view.dart
+++ src/app_feup/lib/view/Pages/book_details_page_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_redux/flutter_redux.dart';
+import 'package:intl/intl.dart';
 import 'package:uni/model/app_state.dart';
 import 'package:uni/model/entities/book.dart';
 import 'package:uni/view/Pages/secondary_page_view.dart';
@@ -269,8 +270,32 @@ class BookDetailsWidget extends StatelessWidget {
       builder: (context) => AlertDialog(
         title: Text('Book Reservation'),
         content: TextField(
-          autofocus: true,
-          decoration: InputDecoration(hintText: 'Testing'),
+          //controller: dateinput, //editing controller of this TextField
+          decoration: InputDecoration(
+              icon: Icon(Icons.calendar_today), //icon of text field
+              labelText: "Enter Date" //label text of field
+          ),
+          readOnly: true,  //set it true, so that user will not able to edit text
+          onTap: () async {
+            DateTime pickedDate = await showDatePicker(
+                context: context, initialDate: DateTime.now(),
+                firstDate: DateTime(2000), //DateTime.now() - not to allow to choose before today.
+                lastDate: DateTime(2101)
+            );
+
+            if(pickedDate != null ){
+              print(pickedDate);  //pickedDate output format => 2021-03-10 00:00:00.000
+              String formattedDate = DateFormat('yyyy-MM-dd').format(pickedDate);
+              print(formattedDate); //formatted date output using intl package =>  2021-03-16
+              //you can implement different kind of Date Format here according to your requirement
+
+              /*setState(() {
+                dateinput.text = formattedDate; //set output date to TextField value.
+              });*/
+            }else{
+              print("Date is not selected");
+            }
+          },
         ),
         actions: [
           TextButton(
