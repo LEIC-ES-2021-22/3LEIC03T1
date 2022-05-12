897bd14d8be72204ba222d5d7207e13ddbb7ac7d
Victor Nunes <victorsnunes99@gmail.com>
2022-05-12 11:20:17 +0100

    form structure done

--- src/app_feup/lib/view/Widgets/book_reservation_dialog.dart
+++ src/app_feup/lib/view/Widgets/book_reservation_dialog.dart
@@ -14,13 +14,125 @@ class bookReservationDialog extends StatefulWidget {
 }
 
 class _bookReservationDialogState extends State<bookReservationDialog> {
-  TextEditingController dateinput = TextEditingController();
 
-  //text editing controller for text field
+  TextEditingController begin_date_controller = TextEditingController();
+  TextEditingController end_date_controller = TextEditingController();
+
+  String _begin_date;
+  String _end_date;
+  bool _is_urgent;
+
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  Widget _buildBeginDateField() {
+    return TextFormField(
+      controller: begin_date_controller,
+      //editing controller of this TextField
+      decoration: InputDecoration(
+          icon: Icon(Icons.calendar_today, color: Colors.grey),
+          labelText: "Data de Início",
+          focusedBorder: UnderlineInputBorder(
+              borderSide: BorderSide(color: Colors.grey, width: 1)
+          )
+      ),
+      validator: (String value) {
+        if (value.isEmpty) {
+          return 'Este campo é obrigatório';
+        }
+        return null;
+      },
+      onSaved: (String value) {
+        _begin_date = value;
+      },
+      readOnly: true,
+      //set it true, so that user will not able to edit text
+      onTap: () async {
+        DateTime pickedDate = await showDatePicker(
+            context: context,
+            initialDate: DateTime.now(),
+            firstDate: DateTime.now(),
+            //DateTime.now() - not to allow to choose before today.
+            lastDate: DateTime(DateTime.now().year + 1)
+        );
+
+        if (pickedDate != null) {
+          print(
+              pickedDate); //pickedDate output format => 2021-03-10 00:00:00.000
+          String formattedDate = DateFormat('dd-MM-yyyy').format(
+              pickedDate);
+          print(
+              formattedDate); //formatted date output using intl package =>  2021-03-16
+          //you can implement different kind of Date Format here according to your requirement
+
+          setState(() {
+            begin_date_controller.text =
+                formattedDate; //set output date to TextField value.
+          });
+        } else {
+          print("Date is not selected");
+        }
+      },
+    );
+  }
+
+  Widget _buildEndDateField() {
+    return TextFormField(
+      controller: end_date_controller,
+      //editing controller of this TextField
+      decoration: InputDecoration(
+          icon: Icon(Icons.calendar_today, color: Colors.grey),
+          labelText: "Data de Devolução",
+          focusedBorder: UnderlineInputBorder(
+              borderSide: BorderSide(color: Colors.grey, width: 1)
+          )
+      ),
+      validator: (String value) {
+        if (value.isEmpty) {
+          return 'Este campo é obrigatório';
+        }
+        return null;
+      },
+      onSaved: (String value) {
+        _begin_date = value;
+      },
+      readOnly: true,
+      //set it true, so that user will not able to edit text
+      onTap: () async {
+        DateTime pickedDate = await showDatePicker(
+            context: context,
+            initialDate: DateTime.now(),
+            firstDate: DateTime.now(),
+            //DateTime.now() - not to allow to choose before today.
+            lastDate: DateTime(DateTime.now().year + 1)
+        );
+
+        if (pickedDate != null) {
+          print(
+              pickedDate); //pickedDate output format => 2021-03-10 00:00:00.000
+          String formattedDate = DateFormat('dd-MM-yyyy').format(
+              pickedDate);
+          print(
+              formattedDate); //formatted date output using intl package =>  2021-03-16
+          //you can implement different kind of Date Format here according to your requirement
+
+
+          setState(() {
+            end_date_controller.text =
+                formattedDate; //set output date to TextField value.
+          });
+        } else {
+          print("Date is not selected");
+        }
+      },
+    );
+  }
+
+  Widget _buildIsUrgentField() {
+    return null;
+  }
 
   @override
   void initState() {
-    dateinput.text = "Escolha uma data"; //set the initial value of text field
     super.initState();
   }
 
@@ -28,60 +140,35 @@ class _bookReservationDialogState extends State<bookReservationDialog> {
   Widget build(BuildContext context) {
     return AlertDialog(
       title: Center(child: Text('Reserva do Livro')),
-      content: Container(
-          padding: EdgeInsets.all(15),
-          height: 150,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                  "Data Inicial"
-              ),
-              TextFormField(
-                controller: dateinput,
-                //editing controller of this TextField
-                decoration: InputDecoration(
-                    icon: Icon(Icons.calendar_today, color: Colors.grey), //icon of text field
-                  focusedBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(color: Colors.grey, width: 1)
-                  )
-                ),
-                readOnly: true,
-                //set it true, so that user will not able to edit text
-                onTap: () async {
-                  DateTime pickedDate = await showDatePicker(
-                      context: context,
-                      initialDate: DateTime.now(),
-                      firstDate: DateTime.now(),
-                      //DateTime.now() - not to allow to choose before today.
-                      lastDate: DateTime(DateTime.now().year + 1)
-                  );
-
-                  if (pickedDate != null) {
-                    print(
-                        pickedDate); //pickedDate output format => 2021-03-10 00:00:00.000
-                    String formattedDate = DateFormat('dd-MM-yyyy').format(
-                        pickedDate);
-                    print(
-                        formattedDate); //formatted date output using intl package =>  2021-03-16
-                    //you can implement different kind of Date Format here according to your requirement
-
-
-                    setState(() {
-                      dateinput.text =
-                          formattedDate; //set output date to TextField value.
-                    });
-                  } else {
-                    print("Date is not selected");
-                  }
-                },
-              ),
-            ],
-          )),
+      content: Form(
+        key: _formKey,
+        child: Container(
+            padding: EdgeInsets.all(15),
+            height: 200,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                _buildBeginDateField(),
+                _buildEndDateField(),
+              ],
+            )
+        ),
+      ),
       actions: [
         TextButton(
           child: Text('SUBMIT'),
           onPressed: () {
+            if (!_formKey.currentState.validate()) {
+              return;
+            }
+
+            _formKey.currentState.save();
+
+            print(_begin_date);
+            print(_end_date);
+            print(_is_urgent);
+
             //TODO: Book Reservation Logic
             Navigator.of(context).pop();
           },
