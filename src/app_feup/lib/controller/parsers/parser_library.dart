4444f610f1acc95c1cc5cda6382ff3d6b3de9717
Bruno Rosendo <brunomrosendo@hotmail.com>
2022-05-17 08:58:02 +0100

    Moved Initial book setter to reLogin function

---
+++ src/app_feup/lib/controller/parsers/parser_library.dart
@@ -0,0 +1,13 @@
+import 'dart:async';
+
+import 'package:http/http.dart' as http;
+import 'package:html/parser.dart' show parse;
+import 'package:html/dom.dart';
+
+//import 'package:web_scraper/web_scraper.dart';
+
+Future<List<Book>> getBooksFromHtml(http.Response response) async {
+  
+
+}
+
